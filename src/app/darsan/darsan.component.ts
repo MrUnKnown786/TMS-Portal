9a9a876b1d381844b523a71bf65f00bbfc387f22
Sanjay Kumar <sanjaypilla786@gmail.com>
2024-04-03 14:54:07 -0400

    Forms

--- src/app/darsan/darsan.component.ts
+++ src/app/darsan/darsan.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl } from '@angular/forms';
@@ -10,10 +10,22 @@ import { FormBuilder, FormControl } from '@angular/forms';
 })
 export class DarsanComponent {
 
-
   templeNames = ['Temple1', 'Temple2', 'Temple3'];
-  darshanTypes = ['Type1', 'Type2'];
+  darshanTypes = ['Type1', 'Type2', 'Type3'];
   selectedValue:any = 0;
+  darshanBooking = this.formBuilder.group({
+    templeName : new FormControl(0),
+    date: new FormControl(),
+    numberofpersons: new FormControl(1),
+    mobile: new FormControl(),
+    name: new FormControl(),
+    dob: new FormControl(),
+    gender: new FormControl('none'),
+    idtype: new FormControl('none'),
+    idnumber: new FormControl(),
+    darshanType: new FormControl()
+
+  });
 
   constructor(private appComponent:AppComponent, private router:Router,private formBuilder:FormBuilder){
     if(!this.appComponent.isLoggedIn){
@@ -22,12 +34,13 @@ export class DarsanComponent {
 
   }
 
-  darshanBooking = this.formBuilder.group({
-    templeName : new FormControl(0)
-  });
+
 
   dropDownChange(){
     this.selectedValue = this.darshanBooking.value.templeName;
   }
 
+  OnSubmit(){
+    console.log(this.darshanBooking.value);
+  }
 }
--- src/app/sevas/sevas.component.ts
+++ src/app/sevas/sevas.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sevas',
@@ -9,11 +10,39 @@ import { Router } from '@angular/router';
 })
 export class SevasComponent {
 
-  constructor(private appComponent:AppComponent, private router:Router){
+  templeNames = ['Temple1', 'Temple2', 'Temple3'];
+  sevaTypes = ['Type1', 'Type2', 'Type3'];
+  selectedValue:any = 0;
+  darshanBooking = this.formBuilder.group({
+    templeName : new FormControl(0),
+    date: new FormControl(),
+    numberofpersons: new FormControl(1),
+    mobile: new FormControl(),
+    name: new FormControl(),
+    dob: new FormControl(),
+    gender: new FormControl('none'),
+    idtype: new FormControl('none'),
+    idnumber: new FormControl(),
+    sevaType: new FormControl(),
+    gothram: new FormControl()
+
+  });
+
+  constructor(private appComponent:AppComponent, private router:Router,private formBuilder:FormBuilder){
     if(!this.appComponent.isLoggedIn){
       this.router.navigate(['login']);
     }
 
+  }
+
+
+
+  dropDownChange(){
+    this.selectedValue = this.darshanBooking.value.templeName;
+  }
+
+  OnSubmit(){
+    console.log(this.darshanBooking.value);
   }
 
 }
